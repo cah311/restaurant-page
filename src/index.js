67a77b8428b1273af3326f0b7ce511e61d4310a4
Carson <c_halter@live.ca>
2022-09-21 14:45:04 -0600

    get logo uploaded

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import _ from "lodash";
+import "./style.css";
+import Logo from "./logo.png";
 
 function component() {
   const element = document.createElement("div");
 
   element.innerHTML = _.join(["Hello", "webpack"], "");
+  element.classList.add("hello");
+
+  const myLogo = new Image();
+  myLogo.src = Logo;
+
+  element.appendChild(myLogo);
 
   return element;
 }
